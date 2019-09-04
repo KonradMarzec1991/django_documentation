07249b67e5295535f69e193ef6bd62a8e3c7fb76
KonradMarzec1991 <konrimarzec@gmail.com>
2019-09-04 22:34:10 +0200

    remove unecessary files

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect  # Http404
 from django.urls import reverse
 from django.views import generic
 from polls.models import Question, Choice
+from django.utils import timezone
 
 
 # def index(request):
@@ -19,7 +20,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 # def detail(request, question_id):
@@ -34,6 +37,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 # def results(request, question_id):
